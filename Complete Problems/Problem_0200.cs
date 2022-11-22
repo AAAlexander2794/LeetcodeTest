e448fbb6a924779189ee7ce6768c1fd074bf6fbf
Alexander <aaalexander2794@gmail.com>
2022-11-22 17:46:51 +0300

    День 9 решен (литкод 75, 1 уровень).

---
+++ Complete Problems/Problem_0200.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0200
+    {
+        public class Solution
+        {
+            public int NumIslands(char[][] grid)
+            {
+                var result = 0;
+                //
+                for (int i = 0; i < grid.Length; i++)
+                {
+                    for (int j = 0; j < grid[i].Length; j++)
+                    {
+                        result = result + ChekIsland(grid, i, j);
+                        //Console.Write($"{grid[i][j]} ");
+                    }
+                    //Console.Write($"\n");
+                }
+                return result;
+            }
+
+            int ChekIsland(char[][] grid, int i, int j)
+            {
+                if (grid[i][j] != '1') return 0;
+                grid[i][j] = '2';
+
+                if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+                    //Console.Write($"up");
+                    ChekIsland(grid, i - 1, j);
+                }
+                if (i + 1 < grid.Length && grid[i + 1][j] == '1') {
+                    //Console.Write($"down");
+                    ChekIsland(grid, i + 1, j);
+                }
+                if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+                    //Console.Write($"left");
+                    ChekIsland(grid, i, j - 1);
+                }
+                if (j + 1 < grid[0].Length && grid[i][j + 1] == '1') {
+                    //Console.Write($"right");
+                    ChekIsland(grid, i, j + 1);
+                }
+                //Console.Write($" +1 ");
+                return 1;
+
+            }
+        }
+    }
+}
---
+++ Complete Problems/Problem_0733.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0733
+    {
+        public class Solution
+        {
+            public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+            {
+                if (image[sr][sc] == color) return image;
+                //
+                var baseColor = image[sr][sc];
+                //
+                image[sr][sc] = color;
+                //
+                if (sr - 1 >= 0 && image[sr - 1][sc] == baseColor)
+                {
+                    image = FloodFill(image, sr - 1, sc, color);
+                    
+                }
+                if (sr + 1 < image.Length && image[sr + 1][sc] == baseColor)
+                {
+                    image = FloodFill(image, sr + 1, sc, color);
+                    
+                }
+                if (sc - 1 >= 0 && image[sr][sc - 1] == baseColor) 
+                {
+                    image = FloodFill(image, sr, sc - 1, color);
+                    
+                }
+                if (sc + 1 < image[0].Length && image[sr][sc + 1] == baseColor) 
+                {
+                    image = FloodFill(image, sr, sc + 1, color);
+
+                }
+                
+                return image;
+            }
+
+            //int[][] Fill(int[][] image, int )
+        }
+    }
+}
