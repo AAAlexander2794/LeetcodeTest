71aa5f8821fed8ddc7a71df69d30786273510210
Alexander <aaalexander2794@gmail.com>
2022-11-22 16:37:03 +0300

    Решил день 8 (это было очень сложно)

---
+++ Complete Problems/Problem_0098.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetcodeTest._Not_Complete_Problems.Problem_0102;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0098
+    {
+       
+ //* Definition for a binary tree node.
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+        public class Solution
+        {
+            public bool IsValidBST(TreeNode root)
+            {
+                return IsValidBstUtil(root, null, null);
+            }
+
+            private bool IsValidBstUtil(TreeNode node, int? min, int? max)
+            {
+                if (node == null)
+                {
+                    return true;
+                }
+                if ((min != null && node.val <= min.Value) || (max != null && node.val >= max.Value))
+                {
+                    return false;
+                }
+                return IsValidBstUtil(node.left, min, node.val) &&
+                        IsValidBstUtil(node.right, node.val, max);
+            }
+        }
+    }
+}
---
+++ Complete Problems/Problem_0235.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0235
+    {
+        
+ // Definition for a binary tree node.
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
+ 
+
+        public class Solution
+        {
+           
+
+            public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+            {
+                if (root == null) return null;
+                if (root.val > p.val && root.val > q.val)
+                {
+                    return LowestCommonAncestor(root.left, p, q);
+                }
+                if (root.val < p.val && root.val < q.val)
+                {
+                    return LowestCommonAncestor(root.right, p, q);
+                }
+                else return root;
+            }
+
+           
+        }
+    }
+}
