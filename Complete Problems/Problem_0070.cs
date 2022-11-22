a15dfcb4836bbcb2c264c83a6798e5bc54a1ce74
Alexander <aaalexander2794@gmail.com>
2022-11-22 18:27:44 +0300

    Day 10 solved.

---
+++ Complete Problems/Problem_0070.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0070
+    {
+        public class Solution
+        {
+            public int ClimbStairs(int n)
+            {
+                if (n == 1) return 1;
+                int a = 1;
+                int b = 2;
+                int result = b;
+                Console.Write($"{a} {b} {result}\n");
+                for (int i = 3; i <= n; i++)
+                {
+
+                    result += a;
+                    a = b;
+                    b = result;
+                    Console.Write($"{a} {b} {result}\n");
+
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Complete Problems/Problem_0509.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0509
+    {
+        public class Solution
+        {
+            public int Fib(int n)
+            {
+                
+                int b = 1;
+                if (n == 0) return 0;
+                if (n == 1) return b;
+                int f = b;
+                for (int i = 2; i < n; i++)
+                {
+
+                    f = f + b;
+                    b = f - b;
+                }
+                return f;
+            }
+        }
+    }
+}
