d4a269e5ede1f08383d07d1ffcf256486b42416c
Alexander <aaalexander2794@gmail.com>
2022-11-23 11:59:30 +0300

    Day 11 done.

---
+++ Complete Problems/Problem_0746.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0746
+    {
+        public class Solution
+        {
+            public int MinCostClimbingStairs(int[] cost)
+            {
+                int[] sum = new int[cost.Length];
+                sum[0] = cost[0];
+                sum[1] = cost[1];
+                for (int i = 2; i < cost.Length; i++)
+                {
+                    sum[i] = cost[i] + Math.Min(sum[i-1], sum[i-2]);
+                }
+                
+                return Math.Min(sum[cost.Length - 1], sum[cost.Length - 2]);
+            }
+        }
+    }
+}
---
+++ _Not Complete Problems/Problem_0062.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0062
+    {
+        public class Solution
+        {
+            public int UniquePaths(int m, int n)
+            {
+                int[,] sum = new int[m,n];
+                sum[0, 0] = 1;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i - 1 >= 0) sum[i, j] += sum[i - 1, j];
+                        if (j - 1 >= 0) sum[i, j] += sum[i, j - 1]; 
+                    }
+                }
+                return sum[m - 1, n - 1];
+            }
+
+            //int Fill(int i, int j, int m, int n)
+            //{
+            //    Console.Write($"{i} {j} | ");
+            //    int sum = 0;
+            //    if (i < m)
+            //    {
+            //        sum += Fill(i + 1, j, m, n);
+            //    }
+            //    if (j < n)
+            //    {
+            //        sum += Fill(i, j + 1, m, n);
+            //    }
+            //    return sum;
+            //}
+        }
+    }
+}
