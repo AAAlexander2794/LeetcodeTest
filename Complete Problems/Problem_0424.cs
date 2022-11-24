29b2710685cf13aa6452ccfb8c9f63bcebaf031b
Alexander <aaalexander2794@gmail.com>
2022-11-24 11:45:14 +0300

    Day 12 done.

---
+++ Complete Problems/Problem_0424.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0424
+    {
+        public class Solution
+        {
+            public int CharacterReplacement(string s, int k)
+            {
+
+                // Максимальная длина ну точно не меньше (k + 1)
+                int maxLength = k + 1;
+                //
+
+                int[] chars = new int[26];
+                
+                for (int i = 0; i < maxLength; i++)
+                {
+                    chars[s[i] - 'A']++;
+                }
+
+                //
+                int start = 0;
+                int end = maxLength - 1;
+                while (end < s.Length)
+                {
+                    // Расширили вправо
+                    end++;
+                    if (end < s.Length) chars[s[end] - 'A']++;
+                    // Если результат прежний или  улучшился, то записываем и старт оставляем, где был
+                    if (chars.Sum() - chars.Max() <= k && chars.Sum() >= maxLength)
+                    {
+                        maxLength = chars.Sum();
+                    }
+                    // Если результат хуже, то левый край переезжает
+                    else
+                    {
+                        chars[s[start] - 'A']--;
+                        start++;
+                    }
+                }
+                return maxLength;
+            }
+
+            public int CharacterReplacementLongTime(string s, int k)
+            {
+                
+                // Максимальная длина
+                int maxLength = 0;
+                //
+                //int maxOfSameLetters = 0;
+                //
+                int[] sChars = new int[26];
+                foreach (char c in s)
+                {
+                    sChars[c - 'A']++;
+                    //if (sChars[c - 'A'] > maxOfSameLetters) maxOfSameLetters = sChars[c - 'A'];
+                }
+                //
+                int start = 0;
+                int end = s.Length - 1;
+                while (end - start >= 0)
+                {
+                    for (int i = 0; i < sChars.Length; i++)
+                    {
+                        sChars[i] = 0;
+                    }
+                    for (int i = start; i <= end; i++)
+                    {
+                        sChars[s[i] - 'A']++;
+                    }
+                    while (end < s.Length)
+                    {
+                        Console.WriteLine($"{start} {end}");
+                        if (sChars.Sum() - sChars.Max() <= k && sChars.Sum() > maxLength)
+                        {
+                            maxLength = sChars.Sum();
+                        }
+                        sChars[s[start] - 'A']--;
+                        start++;
+                        end++;
+                        if (end < s.Length) sChars[s[end] - 'A']++;
+                    }
+                    int length = end - start;
+                    start = 0;
+                    end = start + length - 1;
+                }
+                
+
+
+                return maxLength;
+            }
+        }
+    }
+}
---
+++ Complete Problems/Problem_0438.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0438
+    {
+        public class Solution
+        {
+            public IList<int> FindAnagrams(string s, string p)
+            {
+
+                IList<int> result = new List<int>();
+
+                int[] chars = new int[26];
+
+                if (s.Length < p.Length) return result;
+
+                int[] sChars = new int[26];
+                int[] pChars = new int[26];
+
+                for (int i = 0; i < p.Length; i++)
+                {
+                    sChars[s[i] - 'a']++;
+                }
+
+                foreach (var c in p)
+                {
+                    pChars[c - 'a']++;
+                }
+
+                for (int i = 0; i < s.Length - p.Length; i++)
+                {
+                    if (sChars.SequenceEqual(pChars)) result.Add(i);
+                    sChars[s[i] - 'a']--;
+                    sChars[s[i + p.Length] - 'a']++;
+                }
+                if (sChars.SequenceEqual(pChars)) result.Add(i);
+                return result;
+            }
+
+            public IList<int> FindAnagramsLongTime(string s, string p)
+            {
+                List<int> result = new List<int>();
+                for (int i = 0; i < s.Length - p.Length + 1; i++)
+                {
+                    if (p.Contains(s[i]))
+                    {
+                        var chars = p.ToCharArray().ToList();
+                        for (int j = i; j < i + p.Length; j++)
+                        {
+                            // Если не найден, выходим
+                            if (!chars.Remove(s[j])) break;
+                            // Если все буквы закончились, пишем в результат
+                            if (chars.Count == 0) result.Add(i);
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
