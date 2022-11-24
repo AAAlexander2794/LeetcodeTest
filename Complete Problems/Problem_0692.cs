f7b2bcf903d5be1dc7812c858631558ec2b704de
Alexander <aaalexander2794@gmail.com>
2022-11-24 17:43:29 +0300

    Day 15 done. "LeetCode 75 Level 1" is completed.

---
+++ Complete Problems/Problem_0692.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_0692
+    {
+        public class Solution
+        {
+            public IList<string> TopKFrequent(string[] words, int k)
+            {
+                IDictionary<string, int> pairs = new Dictionary<string, int>();
+                foreach (string word in words)
+                {
+                    if (pairs.ContainsKey(word)) pairs[word]++;
+                    else pairs.Add(word, 1);
+                }
+                List<string> list = new List<string>();
+                //
+                while (k > 0)
+                {
+                    var temp = pairs.Where(x => x.Value == pairs.Values.Max());
+                    //foreach (var item in temp) Console.WriteLine($"{item}");
+                    //Console.WriteLine(k);
+                    List<string> tempList = new List<string>();
+                    foreach (var item in temp) tempList.Add(item.Key);
+                    //foreach (var item in tempList) Console.WriteLine($"{item}");
+
+                    //Console.WriteLine("***");
+                    //foreach (var item in tempList) Console.WriteLine($"{item}");
+                    //Console.WriteLine(k);
+                    tempList.Sort();
+                    //foreach (var item in tempList) Console.WriteLine($"{item}");
+                    //Console.WriteLine(k);
+
+                    foreach (var item in tempList)
+                    {
+                        if (k > 0)
+                        {
+                            list.Add(item);
+                            pairs.Remove(item);
+                            k--;
+                        }
+                        else return list;
+                    }
+                    //foreach (var item in pairs) Console.WriteLine($"{item.Value} {item.Key}");
+                }
+
+                return list;
+            }
+        }
+    }
+}
---
+++ Complete Problems/Problem_1046.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems
+{
+    internal class Problem_1046
+    {
+        public class Solution
+        {
+            public int LastStoneWeight(int[] stones)
+            {
+                var list = stones.ToList();
+                while (list.Count > 1)
+                {
+                    var y = list.Max();
+                    list.Remove(y);
+                    var x = list.Max();
+                    list.Remove(x);
+                    if (y - x > 0) list.Add(y - x);
+                }
+                if (list.Count == 0) return 0;
+                return list.First();
+            }
+        }
+    }
+}
