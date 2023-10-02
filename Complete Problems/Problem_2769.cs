294e68f88a57f29c9ca9eb38fc0b5e47788254c0
Alexander <aaalexander2794@gmail.com>
2023-10-02 14:54:22 +0300

    Решена одна легкая задачка.

---
+++ Complete Problems/Problem_2769.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_2769
+    {
+        public class Solution
+        {
+            public int TheMaximumAchievableX(int num, int t)
+            {
+                return num + t * 2;
+            }
+        }
+    }
+}
