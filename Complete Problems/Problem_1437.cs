e9a707d71a03d8f14ee7729f50bb41f657aa3a38
Alexander <AAAlexander2794@gmail.com>
2025-11-17 19:29:17 +0300

    +1

---
+++ Complete Problems/Problem_1437.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_1437
+    {
+        public class Solution
+        {
+            public bool KLengthApart(int[] nums, int k)
+            {
+                int count = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == 1)
+                    {
+                        if (count > 0) { return false; }
+                        count = k;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
