752e1a71586f8d1ec3447bab21c1d2a2e4dd1251
Alexander <AAAlexander2794@gmail.com>
2025-11-18 20:28:10 +0300

    +1

---
+++ Complete Problems/Problem_0717.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_0717
+    {
+        public class Solution
+        {
+            public bool IsOneBitCharacter(int[] bits)
+            {
+                int i = 0;
+                while (i < bits.Length - 1)
+                {
+                    if (bits[i] == 1) { i += 2; }
+                    else { i++; }
+                }
+                if (i == bits.Length) { return false; }
+                return true;
+            }
+        }
+    }
+}
