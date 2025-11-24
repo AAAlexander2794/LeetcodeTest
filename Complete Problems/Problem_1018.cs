ab6b9d786201193d7087eb6d2ac625f9a19d8c81
Alexander <AAAlexander2794@gmail.com>
2025-11-24 19:59:15 +0300

    +1

---
+++ Complete Problems/Problem_1018.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_1018
+    {
+        public class Solution
+        {
+            public IList<bool> PrefixesDivBy5(int[] nums)
+            {
+                List<bool> bools = new List<bool>();
+                int number = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    number = number * 2 + nums[i];
+                    bools.Add(number % 5 == 0);
+                    number = number % 5;
+                }
+                return bools;
+            }
+        }
+    }
+}
