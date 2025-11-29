de6a9d21285b306a6587f1bb96547b9e10e5714c
Alexander <AAAlexander2794@gmail.com>
2025-11-29 16:56:30 +0300

    +1

---
+++ Complete Problems/Problem_3512.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_3512
+    {
+        public class Solution
+        {
+            public int MinOperations(int[] nums, int k)
+            {
+                int sum = 0;
+                foreach (int i in nums) 
+                {
+                    sum = (sum + i) % k;
+                }
+                return sum;
+            }
+        }
+    }
+}
