04533dccb897c58bbf8d0dfbc05ef52cf215119e
Alexander <AAAlexander2794@gmail.com>
2025-12-05 17:37:44 +0300

    +1

---
+++ Complete Problems/Problem_3432.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_3432
+    {
+        public class Solution
+        {
+            public int CountPartitions(int[] nums)
+            {
+                int countEven = 0;
+                //
+                int sumLeft = 0;
+                int sumRight = 0;
+                // Ищем сумму справа
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    sumRight += nums[i];
+                }
+                //
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    sumLeft += nums[i];
+                    sumRight -= nums[i];
+                    //
+                    if ((sumLeft + sumRight) % 2 == 0)
+                    {
+                        countEven++;
+                    }
+                }
+                return countEven;
+            }
+        }
+    }
+}
