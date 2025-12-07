bf6069c42f986a896d09db836a1d19fe92b62e8a
Alexander <AAAlexander2794@gmail.com>
2025-12-07 11:55:24 +0300

    +1

---
+++ Complete Problems/Problem_1523.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Easy
+{
+    internal class Problem_1523
+    {
+        public class Solution
+        {
+            public int CountOdds(int low, int high)
+            {
+                if (low % 2 == 1 && high % 2 == 1)
+                {
+                    return (high - low) / 2 + 1;
+                }
+                if (low % 2 == 0 && high % 2 == 0)
+                {
+                    return (high - low) / 2;
+                }
+                return (high - low + 1) / 2;
+            }
+        }
+    }
+}
