8088e60aabeb3cddb3ae072460fcd37f74ce515f
Alexander <AAAlexander2794@gmail.com>
2026-01-06 15:13:58 +0300

    +

---
+++ Complete Problems/Problem_2161.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Medium
+{
+    internal class Problem_2161
+    {
+        public class Solution
+        {
+            public int[] PivotArray(int[] nums, int pivot)
+            {
+                List<int> beforePivot = new List<int>();
+                List<int> inPivot = new List<int>();
+                List<int> afterPivot = new List<int>();
+                foreach (int num in nums)
+                {
+                    if (num < pivot)
+                    {
+                        beforePivot.Add(num);
+                        continue;
+                    }
+                    if (num > pivot)
+                    {
+                        afterPivot.Add(num);
+                        continue;
+                    }
+                    inPivot.Add(num);
+                }
+                int[] res = new int[nums.Length];
+                int i = 0;
+                foreach (int num in beforePivot)
+                {
+                    res[i++] = num;
+                }
+                foreach (int num in inPivot)
+                {
+                    res[i++] = num;
+                }
+                foreach (int num in afterPivot)
+                {
+                    res[i++] = num;
+                }
+                return res;
+            }
+        }
+    }
+}
---
+++ _Not Complete Problems/Medium/Problem_1161.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeTest._Not_Complete_Problems.Medium
+{
+    internal class Problem_1161
+    {
+
+        //Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            //public int MaxLevelSum(TreeNode root)
+            //{
+            //    int level = 1;
+            //    int max = root.val;
+
+            //}
+        }
+    }
+}
